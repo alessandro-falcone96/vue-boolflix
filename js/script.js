bd25602b1944acd94a53c6bef77bc0a4ba96949c
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-09 14:50:37 +0200

    Aggiunto v-model per la ricerca dei titoli sia con input sia con button

--- js/script.js
+++ js/script.js
@@ -5,31 +5,32 @@ var app = new Vue (
     el: "#app",
     data: {
       film: [],
-      link: "https://image.tmdb.org/t/p/original/"
+      link: "https://image.tmdb.org/t/p/original/",
+      queryInput: ""
     },
-    mounted: function() {
-      {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-          params: {
-            api_key: "d1f8d7650c9da069b8dc77f3607078db",
-            query: "Harry Potter",
-          }
-        })
-        .then((risposta) => {
+    methods: {
+      search: function() {
+        this.film = [];
+        {
+          axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+              api_key: "d1f8d7650c9da069b8dc77f3607078db",
+              query: this.queryInput,
+            }
+          })
+          .then((risposta) => {
 
-          var oggetto = risposta.data.results;
-          this.film.push(oggetto);
-          console.log(this.film);
+            var oggetto = risposta.data.results;
+            this.film.push(oggetto);
+            console.log(this.film);
 
-          // for (var variable in this.film) {
-          //   this.link += "ciao";
-          // }
-
-        })
-
-
-
+          })
+          this.queryInput = "";
+        }
       }
-    }
+    },
+    // mounted: function() {
+    //
+    // }
   }
 );
