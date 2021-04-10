bef7a8735b0eb1d2cd6fcccb6cf81615d8b17eb9
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-10 16:09:57 +0200

    Aggiunte le stelle dei voti

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@ var app = new Vue (
     data: {
       film: [],
       link: "https://image.tmdb.org/t/p/original/",
-      queryInput: ""
+      queryInput: "",
+      titoloPrincipale: "",
+      // filmPreferiti: ["Iron Man", "L'Incredibile Hulk", "Iron Man 2", "Thor", "Captain America - Il Primo Vendicatore", "The Avengers", "Iron Man 3", " Thor: The Dark World", "Captain America: The Winter Soldier", "Guardiani della galassia", "Avengers: Age of Ultron", "Ant-Man", "Captain America: Civil War", "Doctor Strange", "Guardiani della Galassia Vol 2", "Spider-Man: Homecoming", "Thor: Ragnarok", "Black Panther", "Avengers: Infinity War", "Ant-Man and the Wasp", "Captain Marvel", "Avengers: Endgame", "Spider-Man: Far From Home"]
     },
     methods: {
       search: function() {
         this.film = [];
+        this.titoloPrincipale = "Titoli filtrati per '" + this.queryInput + "'";
         {
           axios.get('https://api.themoviedb.org/3/search/movie', {
             params: {
@@ -22,15 +25,27 @@ var app = new Vue (
 
             var oggetto = risposta.data.results;
             this.film.push(oggetto);
-            console.log(this.film);
+            // console.log(this.film);
+
+
+            // Trasformazione voto da 10 a 5
+            let voto = null;
+            for (var item in oggetto) {
+              voto = oggetto[item].vote_average;
+              let newVoto = voto / 2;
+              let decimale = newVoto % 1;
+              newVoto = Math.floor(newVoto);
+              if (decimale > 0.5) {
+                newVoto += 1;
+              }
+              oggetto[item].vote_average = newVoto;
+            }
 
           })
           this.queryInput = "";
         }
+
       }
     },
-    // mounted: function() {
-    //
-    // }
   }
 );
